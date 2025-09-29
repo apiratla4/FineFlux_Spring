39a5b11b56417894e06b2c15f0eba0279bd393ec
dhanunjay20 <dhanunjay@hiresoftsolutions.com>
2025-09-29 12:17:27 +0530

    initial commit

---
+++ src/main/java/com/pulse/fineflux/FinefluxApplication.java
@@ -0,0 +1,13 @@
+package com.pulse.fineflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinefluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinefluxApplication.class, args);
+	}
+
+}
